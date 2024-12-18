a4cbeb6bf9b2d08430b380e9ed845adb4423ed38
Jhojan98 <jhojanramirez75@gmail.com>
2024-12-18 11:54:23 -0500

    Creacion
    De momento estoy implementando las clases propuestas, en el documento de habitabilidad

---
+++ modelado/classPared.py
@@ -0,0 +1,8 @@
+class Pared:
+    def __init__(self, material, espacio_1,espacio_2):
+        self.material= material
+        self.espacio_1= espacio_1
+        self.espacio_2 = espacio_2
+        
+    def conectar():
+        return False
