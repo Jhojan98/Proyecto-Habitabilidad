c26dcc4956a3abab77ac1ddb764607383c329344
Jhojan98 <jhojanramirez75@gmail.com>
2025-01-07 12:51:04 -0500

    Continuando creacion de instancias
    nada importante, se elimino toda la info de matrizEdificio todavia nose que poner ahi xd

--- modelado/classPared.py
+++ modelado/classPared.py
@@ -2,12 +2,12 @@ from modelado.classMaterial import Material
 from modelado.classEspacios import Espacios
 
 class Pared:
-    def __init__(self, material:Material, espacio_1:Espacios ,espacio_2:Espacios):
-        self.material= material
-        self.espacio_1= espacio_1
-        self.espacio_2 = espacio_2
+    def __init__(self, id_material: int, id_espacio_1: int, id_espacio_2: int):
+        self.id_material = id_material
+        self.id_espacio_1 = id_espacio_1
+        self.id_espacio_2 = id_espacio_2
         
-    def conectar():
+    def conectar(self):
         """
         Conecta dos espacios
         """
@@ -15,19 +15,20 @@ class Pared:
     
     # Getters y Setters
     
-    def get_material(self) -> Material:
-        return self.material
-    def set_material(self, material: Material):
-        self.material = material
+    def get_id_material(self) -> int:
+        return self.id_material
 
-    def get_espacio_1(self) -> Espacios:
-        return self.espacio_1
+    def set_id_material(self, id_material: int):
+        self.id_material = id_material
 
-    def set_espacio_1(self, espacio_1: Espacios):
-        self.espacio_1 = espacio_1
+    def get_id_espacio_1(self) -> int:
+        return self.id_espacio_1
 
-    def get_espacio_2(self) -> Espacios:
-        return self.espacio_2
+    def set_id_espacio_1(self, id_espacio_1: int):
+        self.id_espacio_1 = id_espacio_1
 
-    def set_espacio_2(self, espacio_2: Espacios):
-        self.espacio_2 = espacio_2
+    def get_id_espacio_2(self) -> int:
+        return self.id_espacio_2
+
+    def set_id_espacio_2(self, id_espacio_2: int):
+        self.id_espacio_2 = id_espacio_2
